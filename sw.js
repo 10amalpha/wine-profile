d5ec49462cc10444d4ccbf2e56dce6c81c67ea8a
10ampro <info@10am.pro>
2026-02-08 13:22:18 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hedonicum-v1';
+const CACHE = 'hedonicum-v2';
 const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
